1f8041b614aed726c5882804be0486a5f5dd04d5
Thomas A Caswell <tcaswell@bnl.gov>
2018-03-23 18:22:40 -0400

    TST: make the caproto layer super verbose

--- ophyd/_caproto_shim.py
+++ ophyd/_caproto_shim.py
@@ -1,5 +1,21 @@
 import threading
-from caproto.threading.client import caget, caput, get_pv
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s')
+
+from caproto.threading.client import PVContext
+
+ctx = PVContext(log_level='DEBUG')
+
+def get_pv(*args, **kwargs):
+    return ctx.get_pv(*args, **kwargs)
+
+
+def caget(pvname, *args, **kwargs):
+    return get_pv(pvname).get(*args, **kwargs)
+
+
+def caput(pvname, *args, **kwargs):
+    return get_pv(pvname).put(*args, **kwargs)
 
 thread_class = threading.Thread
 pv_form = 'time'
