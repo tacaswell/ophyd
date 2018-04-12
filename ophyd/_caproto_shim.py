b0b9cce1909d4e665e08c3b87ae4b8c875fb477a
Thomas A Caswell <tcaswell@bnl.gov>
2018-04-12 14:54:03 -0400

    MNT: update for some renames

--- ophyd/_caproto_shim.py
+++ ophyd/_caproto_shim.py
@@ -2,20 +2,7 @@ import threading
 import logging
 logging.basicConfig(format='%(asctime)s %(message)s')
 
-from caproto.threading.client import PVContext
-
-ctx = PVContext(log_level='DEBUG')
-
-def get_pv(*args, **kwargs):
-    return ctx.get_pv(*args, **kwargs)
-
-
-def caget(pvname, *args, **kwargs):
-    return get_pv(pvname).get(*args, **kwargs)
-
-
-def caput(pvname, *args, **kwargs):
-    return get_pv(pvname).put(*args, **kwargs)
+from caproto.threading.pyepics_compat import get_pv, caput, caget
 
 thread_class = threading.Thread
 pv_form = 'time'
