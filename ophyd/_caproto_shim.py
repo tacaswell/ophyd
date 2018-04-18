08cb66551e666db4eb6daab2e173decc693cb80b
Thomas A Caswell <tcaswell@bnl.gov>
2018-04-18 17:23:39 -0400

    TST: turn down logging
    The fail logs are over 4MB on travis and get cutoff!

--- ophyd/_caproto_shim.py
+++ ophyd/_caproto_shim.py
@@ -1,7 +1,4 @@
 import threading
-import logging
-logging.basicConfig(format='%(asctime)s %(message)s')
-
 from caproto.threading.pyepics_compat import get_pv, caput, caget
 
 thread_class = threading.Thread
